ad2576f03191e6f823b43baee9638aed0499339c
Cheolhee KIM(김철희) [DTWORESOURCE] <chkim@dtwocorp.com>
2023-10-06 18:24:54 +0900

    가위바위보 구현 완료

---
+++ 5.가위바위보/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+import RockScissorsPaper from './RockScissorsPaper';
+
+new Vue(RockScissorsPaper).$mount('#root'); 
+// new Vue({
+//     render: h => h(ResponseCheck),
+//   }).$mount('#root');
