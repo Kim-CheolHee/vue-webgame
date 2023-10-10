cd862ed740980a5410cfeae0867454d164facccd
Cheolhee KIM(김철희) [DTWORESOURCE] <chkim@dtwocorp.com>
2023-10-10 12:32:52 +0900

    로또 추첨기 구현 완료

--- 6.로또/LottoBall.vue
+++ 6.로또/LottoBall.vue
@@ -1,18 +1,34 @@
 <template>
-    <div>
-
-    </div>
+    <div class="ball" :style="background">{{ number }}</div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-
-        }
+    name: 'LottoBall',
+    // props는 부모 컴포넌트에서 자식 컴포넌트로 데이터를 전달할 때 사용하는 속성
+    // props는 부모 컴포넌트에서 전달한 데이터를 자식 컴포넌트에서 변경할 수 없다. read-only 느낌
+    props: {
+        number: Number,
     },
+    // 기존 data를 통해 새로운 데이터를 만들어내는 computed
     computed: {
-
+        background () {
+            let background
+            if (this.number <= 10) {
+                background = 'red'
+            } else if (this.number <= 20) {
+                background = 'orange'
+            } else if (this.number <= 30) {
+                background = 'yellow'
+            } else if (this.number <= 40) {
+                background = 'blue'
+            } else {
+                background = 'green'
+            }
+            return {
+                background,
+            }
+        }
     },
     methods: {  
 
@@ -21,5 +37,15 @@ export default {
 </script>
 
 <style scoped>
-
+    .ball {
+        display: inline-block;
+        border: 1px solid black;
+        border-radius: 20px;
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        margin-right: 10px;
+        font-size: 20px;
+    }
 </style>
--- 6.로또/LottoGenerator.vue
+++ 6.로또/LottoGenerator.vue
@@ -2,18 +2,40 @@
     <div>
         <div>당첨 숫자</div>
         <div id="결과창">
-            <lotto-ball v-for="(b, index) in winBalls" :key="index"></lotto-ball>
+            <lotto-ball v-for="(b, index) in winBalls" :key="index" :number="b"></lotto-ball>
         </div>
         <div>보너스</div>
-        <lotto-ball v-if="bonus"></lotto-ball>
-        <button v-if="redo">한 번 더!</button> 
+        <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+        <button v-if="redo" @click="onClickRedo">한 번 더!</button> 
     </div>
 </template>
 
 <script>
+import LottoBall from './LottoBall';
+
+function getWinNumbers () {
+    console.log('getWinNumbers');
+    const candidate = Array(45).fill().map((v, i) => i + 1);
+    const shuffle = [];
+    while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length - 1];
+    const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+    return [...winNumbers, bonusNumber];
+}
+
+const timeouts = []
+
 export default {
-    data() {
+    components: {
+        // 컴포넌트가 파스칼 케이스면 자동으로 vue는 케밥 케이스로 변환해준다.
+        // LottoBall: LottoBall,
+        'lotto-ball': LottoBall
+    },
+    data () {
         return {
+            winNumbers: getWinNumbers(), // 당첨 숫자들
             winBalls: [],
             bonus: null,
             redo: false,
@@ -23,17 +45,55 @@ export default {
 
     },
     methods: {  
-
+        onClickRedo () {
+            this.winNumbers = getWinNumbers();
+            this.winBalls = [];
+            this.bonus = null;
+            this.redo = false;
+            this.showBalls();
+        },
+        showBalls () {
+            for (let i = 0; i < this.winNumbers.length - 1; i++) {
+                timeouts[i] = setTimeout(() => {
+                    this.winBalls.push(this.winNumbers[i])
+                }, (i + 1) * 1000);
+            }
+            timeouts[6] = setTimeout(() => {
+                this.bonus = this.winNumbers[6];
+                this.redo = true;
+            }, 7000);
+        }
     },
     mounted() {
-        console.log('LottoBall mounted');
+        this.showBalls();
     },
     beforeDestroy() {
-        console.log('LottoBall beforeDestroy');
+        timeouts.forEach((v) => {
+            clearTimeout(v);
+        });
     },
-    watch() {
-
-    }
+    // watch는 데이터가 변할 때마다 실행되는 함수
+    // watch 사용은 지양하자.
+    // watch: {
+    //     // winBalls (value, oldValue) {
+    //     //     if (value.length === 0) {
+    //     //         this.showBalls();
+    //     //     }
+    //     //     console.log(value, oldValue);
+    //     // }
+    //     // bonus (value, oldValue) {
+    //     //     if (value === null) {
+    //     //         this.showBalls();
+    //     //     }
+    //     //     console.log(value, oldValue);
+    //     // }
+    //     redo (value, oldValue) {
+    //         if (value === false) {
+    //             this.showBalls();
+    //         }
+    //         console.log(value, oldValue);
+    //     }
+    // }
 }
 </script>
 
