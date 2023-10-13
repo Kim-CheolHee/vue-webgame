9d6ed9df940198d21484cc031d392889a6c1712c
Cheolhee KIM(김철희) [DTWORESOURCE] <chkim@dtwocorp.com>
2023-10-13 14:07:52 +0900

    vue-route 구현완료

---
+++ vue-router/GameMatcher.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div v-if="currentGame === 'index'">
+            <h1>플레이 하고 싶은 게임을 클릭하세요!</h1>
+        </div>
+        <div v-else-if="currentGame === 'number-baseball'">
+            <number-baseball></number-baseball>
+        </div>
+        <div v-else-if="currentGame === 'response-check'">
+            <response-check></response-check>
+        </div>
+        <div v-else-if="currentGame === 'lotto-generator'">
+            <lotto-generator></lotto-generator>
+        </div>
+        <div v-else-if="currentGame === 'rock-scissors-paper'">
+            <rock-scissors-paper></rock-scissors-paper>
+        </div>
+        <div v-else-if="currentGame === 'mine-sweeper'">
+            <mine-sweeper></mine-sweeper>
+        </div>
+        <div v-else>일치하는 게임이 없습니다.</div>
+    </div>
+</template>
+
+<script>
+import NumberBaseball from '../3.숫자야구/NumberBaseball'
+import ResponseCheck from '../4.반응속도체크/ResponseCheck'
+import RockScissorsPaper from '../5.가위바위보/RockScissorsPaper'
+import LottoGenerator from '../6.로또/LottoGenerator'
+import MineSweeper from '../8.지뢰찾기/MineSweeper'
+
+export default {
+    components: {
+        NumberBaseball,
+        ResponseCheck,
+        RockScissorsPaper,
+        LottoGenerator,
+        MineSweeper
+    },
+    mounted () {
+        console.log(this.$router);
+        console.log(this.$route);
+    },
+    watch: {
+        '$route': 'routeChanged'
+    },
+    methods: {
+        routeChanged () {
+            console.log("Route changed:", this.$router, this.$route);
+        }
+    },
+    computed: {
+        currentGame () {
+            return this.$route.params.name;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-router/Router.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div>
+            <router-link to="/game/index">게임 매쳐</router-link>
+            <router-link to="/game/number-baseball?data=1&name=kim&age=32">숫자야구</router-link>
+            <router-link to="/game/response-check">반응속도체크</router-link>
+            <router-link to="/game/rock-scissors-paper">가위바위보</router-link>
+            <router-link to="/game/lotto-generator">로또번호 생성기</router-link>
+            <router-link to="/game/mine-sweeper">지뢰찾기</router-link>
+        </div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import router from './routes'
+import store from './store'
+
+export default {
+    router,
+    store,
+}
+</script>
+
+<style>
+
+</style>
